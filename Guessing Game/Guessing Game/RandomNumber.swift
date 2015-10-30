b38610ccca2b51abbeb6d7e22949234ca1311272
MFrob <mees.froberg@student.uva.nl>
2015-10-30 06:49:51 -0700

    version 3.0

--- Guessing Game/Guessing Game/RandomNumber.swift
+++ Guessing Game/Guessing Game/RandomNumber.swift
@@ -2,6 +2,8 @@
 //  RandomNumber.swift
 //  Guessing Game
 //
+// This class calculates a random integer between 1 and a given range.
+//
 //  Created by Mees Fröberg on 27/10/15.
 //  Copyright © 2015 Mees Fröberg. All rights reserved.
 //
@@ -10,17 +12,20 @@ import Foundation
 
 class RandomNumber {
     var number:Int
-    private let range:Int
+    var range:Int
     
     init(range:Int){
         number = 0
         self.range = range
     }
     
+    // Generate next integer.
     func nextNumber() {
         number = generateRandomInteger(range)
+        print(String(format:"New number is: %d", number))
     }
     
+    // Generate a random integer and return it.
     private func generateRandomInteger(range:Int) -> Int{
         return Int(arc4random_uniform(UInt32(range)))
     }
--- Guessing Game/Guessing Game/ViewController.swift
+++ Guessing Game/Guessing Game/ViewController.swift
@@ -10,41 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Create a randomNumber object to generate random numbers
     let randomNumber = RandomNumber(range: 10)
     
     @IBOutlet weak var score: UILabel!
     @IBOutlet weak var feedback: UILabel!
     @IBOutlet weak var number: UITextField!
+
     
+    // When the range button is pressed, change the range of the randomNumber object.
+    @IBAction func setRange(sender: AnyObject) {
+        if let range = Int(number.text!) {
+            randomNumber.range = range
+            print(String(format:"New range is: %d", range))
+        }
+        number.text = ""
+    }
+    
+    // When the go button is pressed handle the input of the user.
     @IBAction func guess(sender: AnyObject) {
-        if feedback.text == "Guess" {
-            randomNumber.nextNumber()
-            if randomNumber.number == Int(number.text!) {
-                correct()
-            } else if randomNumber.number > Int(number.text!) {
-                feedback.text = "Higher"
+        if let guess = Int(number.text!) {
+            if feedback.text == "Guess" {
+                randomNumber.nextNumber()
+                if randomNumber.number == guess {
+                    correct()
+                } else if randomNumber.number > guess {
+                    feedback.text = "Higher"
+                } else {
+                    feedback.text = "Smaller"
+                }
             } else {
-                feedback.text = "Smaller"
-            }
-        } else {
-            if randomNumber.number == Int(number.text!) {
-                correct()
-            } else {
-                incorrect()
+                if randomNumber.number == guess {
+                    correct()
+                } else {
+                    incorrect()
+                }
             }
         }
         number.text = ""
     }
     
+    // When the input of the user is correct execute this function.
     func correct() {
         score.text = String(Int(score.text!)! + 1)
-//        feedback.text = "Correct!"
         feedback.text = "Guess"
     }
     
+    // When the input of the user is incorrect execute this function.
     func incorrect() {
-//        feedback.text = "Incorrect"
-//        feedback.text = String(format: "Number was %d", randomNumber.number)
         feedback.text = "Guess"
     }
     
